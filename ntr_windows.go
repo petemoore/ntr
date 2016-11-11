2a572cffcd88441e89b93f72902d688f2becc77e
Pete Moore <pmoore@mozilla.com>
2016-11-11 15:34:28 +0100

    additions...

--- ntr_windows.go
+++ ntr_windows.go
@@ -1,6 +1,7 @@
 package ntr
 
 import (
+	"fmt"
 	"syscall"
 	"unsafe"
 )
@@ -8,8 +9,12 @@ import (
 var (
 	advapi32 = syscall.NewLazyDLL("advapi32.dll")
 
-	procLsaOpenPolicy = advapi32.NewProc("LsaOpenPolicy")
+	procLsaAddAccountRights = advapi32.NewProc("LsaAddAccountRights")
+	procLsaOpenPolicy       = advapi32.NewProc("LsaOpenPolicy")
 )
+
+// Refer to https://msdn.microsoft.com/en-us/library/windows/desktop/aa383751(v=vs.85).aspx
+// for understanding the c++ -> go type mappings
 
 type ACCESS_MASK uint32
 
@@ -35,7 +40,7 @@ func LsaOpenPolicy(
 	systemName *LSAUnicodeString, // PLSA_UNICODE_STRING
 	objectAttributes *LSAObjectAttributes, // PLSA_OBJECT_ATTRIBUTES
 	desiredAccess ACCESS_MASK, // ACCESS_MASK
-	policyHandle syscall.Handle, // PLSA_HANDLE
+	policyHandle *syscall.Handle, // PLSA_HANDLE in/out
 ) (err error) {
 	r1, _, e1 := syscall.Syscall6(
 		procLsaOpenPolicy.Addr(),
@@ -43,7 +48,7 @@ func LsaOpenPolicy(
 		uintptr(unsafe.Pointer(systemName)),
 		uintptr(unsafe.Pointer(objectAttributes)),
 		uintptr(desiredAccess),
-		uintptr(policyHandle),
+		uintptr(unsafe.Pointer(policyHandle)),
 		0,
 		0,
 	)
@@ -55,4 +60,51 @@ func LsaOpenPolicy(
 		}
 	}
 	return
+}
+
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ms721786(v=vs.85).aspx
+func LsaAddAccountRights(
+	policyHandle syscall.Handle, // LSA_HANDLE
+	accountSid *syscall.SID, // PSID
+	userRights *LSAUnicodeString, // PLSA_UNICODE_STRING
+	countOfRights uint32, // ULONG
+) (err error) {
+	r1, _, e1 := syscall.Syscall6(
+		procLsaAddAccountRights.Addr(),
+		4,
+		uintptr(policyHandle),
+		uintptr(unsafe.Pointer(accountSid)),
+		uintptr(unsafe.Pointer(userRights)),
+		uintptr(countOfRights),
+		0,
+		0,
+	)
+	if r1 == 0 {
+		if e1 != 0 {
+			err = error(e1)
+		} else {
+			err = syscall.EINVAL
+		}
+	}
+	return
+}
+
+// https://msdn.microsoft.com/en-us/library/windows/desktop/ms722492(v=vs.85).aspx
+func LSAUnicodeStringPtrFromStringPtr(s *string) (*LSAUnicodeString, error) {
+	if s == nil {
+		return nil, nil
+	}
+	utf16, err := syscall.UTF16FromString(*s)
+	if err != nil {
+		return nil, err
+	}
+	dwLen := len(utf16)
+	if dwLen > 0x7ffe {
+		return nil, fmt.Errorf("LSA string:\n%v\n\nLSA string too long - it is %v characters, max allowed is 32766.", *s, dwLen)
+	}
+	return &LSAUnicodeString{
+		Length:        uint16(2 * dwLen),
+		MaximumLength: uint16(2*dwLen + 2),
+		Buffer:        &utf16[0],
+	}, nil
 }
